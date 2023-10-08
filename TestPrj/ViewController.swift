c8e93d9f33425aab51a587537f31e5b536b0ba8a
BillMau <billmau123@gmail.com>
2023-10-08 21:48:29 +0800

    print 2

--- TestPrj/ViewController.swift
+++ TestPrj/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("--m hello world")
+        print("--m hello world 2")
         // Do any additional setup after loading the view.
     }
 
